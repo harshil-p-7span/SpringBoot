5ec930736885338c98389c1d8ee8a4dfa63acccf
Harshil <harshil.p@7span.com>
2023-06-13 14:28:28 +0530

    Delete all files.

--- src/main/java/com/example/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository  // Used to mark a class as a repository, typically for database access.
-public interface UserRepository extends JpaRepository<User, Long> {
-}
